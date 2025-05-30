94307095f9bbf6cb624eff25cae55b567f86a61c
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-05-29 23:53:12 -0400

    minor styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,13 @@ export default function Home() {
 
 	return (
 		<div className="p-12">
+			{/* AI CHAT MESSAGES */}
 			<div className="flex flex-col gap-4 mx-auto pb-32 max-w-4xl">
 				{messages.map((message) => {
 					switch (message.role) {
 						case "user":
 							return (
-								<p key={message.id} className="self-end bg-zinc-800 px-4 py-2 rounded-3xl max-w-4/5">{message.content}</p>
+								<p key={message.id} className="self-end bg-zinc-900 px-4 py-2 rounded-3xl max-w-4/5 text-zinc-200">{message.content}</p>
 							)
 						case "assistant":
 							return (
@@ -46,14 +47,10 @@ export default function Home() {
 				})}
 			</div>
 
-			<div className="top-4 left-4 fixed flex flex-col gap-2 bg-zinc-800 p-2 border border-white/15 rounded-2xl">
-				<p className="bg-zinc-700 px-12 py-1 rounded-xl font-mono">TOKENS: {totalTokens}</p>
-				<p className="bg-zinc-700 px-12 py-1 rounded-xl font-mono">STATUS: {status}</p>
-				<div className="bg-white/15 mx-2 h-px"></div>
-				<button className="bg-zinc-700 hover:bg-zinc-600 px-3 py-1 rounded-xl duration-150 cursor-pointer" onClick={() => reload()}>Reload</button>
-				<button className="bg-zinc-700 hover:bg-zinc-600 px-3 py-1 rounded-xl duration-150 cursor-pointer" onClick={() => stop()}>Stop</button>
-			</div>
+			{/* BOTTOM GRADIENT */}
+			<div className="bottom-0 left-0 fixed bg-gradient-to-t from-black to-transparent w-full h-24"></div>
 
+			{/* INPUT AREA */}
 			<form onSubmit={handleSubmit} className="bottom-4 left-1/2 fixed -translate-x-1/2">
 				<motion.input
 					required
@@ -71,6 +68,15 @@ export default function Home() {
 					whileFocus={{ marginBottom: 12, borderColor: "rgba(255, 255, 255, 0.15)" }}
 				/>
 			</form>
+
+			{/* DEBUG INFO */}
+			<div className="top-4 left-4 fixed flex flex-col gap-2 bg-zinc-800 p-2 border border-white/15 rounded-2xl">
+				<p className="bg-zinc-700 px-12 py-1 rounded-xl font-mono">TOKENS: {totalTokens}</p>
+				<p className="bg-zinc-700 px-12 py-1 rounded-xl font-mono">STATUS: {status}</p>
+				<div className="bg-white/15 mx-2 h-px"></div>
+				<button className="bg-zinc-700 hover:bg-zinc-600 px-3 py-1 rounded-xl duration-150 cursor-pointer" onClick={() => reload()}>Reload</button>
+				<button className="bg-zinc-700 hover:bg-zinc-600 px-3 py-1 rounded-xl duration-150 cursor-pointer" onClick={() => stop()}>Stop</button>
+			</div>
 		</div>
 	);
 }
