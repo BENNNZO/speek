54e52cadf5264ac7fc8bd4c3b22aed8496ddd723
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-05-29 22:08:17 -0400

    added more styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,32 +7,29 @@ export default function Home() {
 
 	return (
 		<div className="p-12">
-			<div className="flex flex-col gap-2">
+			<div className="flex flex-col gap-4 mx-auto pb-32 max-w-4xl">
 				{messages.map((message) => {
 					switch (message.role) {
 						case "user":
 							return (
-								<p>{message.content}</p>
+								<p className="self-end bg-zinc-800 px-3 py-2 rounded-full">{message.content}</p>
 							)
 						case "assistant":
 							return (
-								<p className="bg-zinc-800 px-3 py-2 rounded-lg">{message.content}</p>
+								<p className="self-start w-4/5">{message.content}</p>
 							)
 					}
-					// <div key={message.id} className="bg-zinc-800">
-					// 	<p>ROLE: {message.role}</p>
-					// 	<p>CONTENT: {message.content}</p>
-					// </div>
 				})}
 			</div>
 
 			<form onSubmit={handleSubmit} className="bottom-4 left-1/2 fixed -translate-x-1/2">
 				<input
+					autoFocus
 					type="text"
 					placeholder="Ask Anything..."
 					value={input}
 					onChange={handleInputChange}
-					className="bg-zinc-800 px-4 py-2 rounded-full"
+					className="bg-zinc-900 px-4 py-2 border border-white/15 rounded-full"
 				/>
 			</form>
 		</div>
