815acefa429deb21183d4f97a24db685f136e3bd
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-05-29 21:57:53 -0400

    added a bit of style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,33 @@ export default function Home() {
 	const { messages, input, handleInputChange, handleSubmit } = useChat({ api: "api/openai" })
 
 	return (
-		<div>
-			<div>
-				{messages.map((message) => (
-					<div key={message.id}>
-						<p>{message.role}</p>
-						<p>{message.content}</p>
-						{message.parts.map((part) => (
-							<p>{part.type}</p>
-						))}
-					</div>
-				))}
+		<div className="p-12">
+			<div className="flex flex-col gap-2">
+				{messages.map((message) => {
+					switch (message.role) {
+						case "user":
+							return (
+								<p>{message.content}</p>
+							)
+						case "assistant":
+							return (
+								<p className="bg-zinc-800 px-3 py-2 rounded-lg">{message.content}</p>
+							)
+					}
+					// <div key={message.id} className="bg-zinc-800">
+					// 	<p>ROLE: {message.role}</p>
+					// 	<p>CONTENT: {message.content}</p>
+					// </div>
+				})}
 			</div>
 
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleSubmit} className="bottom-4 left-1/2 fixed -translate-x-1/2">
 				<input
 					type="text"
-					placeholder="say something..."
+					placeholder="Ask Anything..."
 					value={input}
 					onChange={handleInputChange}
+					className="bg-zinc-800 px-4 py-2 rounded-full"
 				/>
 			</form>
 		</div>
