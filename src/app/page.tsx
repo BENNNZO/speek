1c10b8573386bcd843370a9a6468f0a2e7cdf737
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-05-29 21:49:56 -0400

    created a very basic user interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client"
+
+import { useChat } from "@ai-sdk/react";
+
 export default function Home() {
+	const { messages, input, handleInputChange, handleSubmit } = useChat({ api: "api/openai" })
+
 	return (
 		<div>
-			Hello, World!
+			<div>
+				{messages.map((message) => (
+					<div key={message.id}>
+						<p>{message.role}</p>
+						<p>{message.content}</p>
+						{message.parts.map((part) => (
+							<p>{part.type}</p>
+						))}
+					</div>
+				))}
+			</div>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="say something..."
+					value={input}
+					onChange={handleInputChange}
+				/>
+			</form>
 		</div>
 	);
 }
