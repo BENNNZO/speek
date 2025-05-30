80fb96e539eb4838e1590bd83de5673bc8eb3ec1
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-05-30 10:25:04 -0400

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,42 +3,29 @@
 import { useChat } from "@ai-sdk/react";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+
+import Messages from "./components/chat/Messages";
 
 export default function Home() {
-	const { messages, input, handleInputChange, handleSubmit, status, reload, stop } = useChat({ api: "api/openai", onFinish: (message, { usage }) => { finishCallback(message.content, usage.totalTokens) } })
-
 	const [totalTokens, setTotalTokens] = useState(0)
 
 	function finishCallback(message: string, usage: number) {
 		setTotalTokens(prev => prev + usage)
 	}
 
+	const { messages, input, handleInputChange, handleSubmit, status, reload, stop } = useChat({
+		api: "api/openai",
+		onFinish: (message, { usage }) => { finishCallback(message.content, usage.totalTokens) }
+	})
+
 	return (
 		<div className="p-12">
 			{/* AI CHAT MESSAGES */}
-			<div className="flex flex-col gap-4 mx-auto pb-32 max-w-4xl">
-				{messages.map((message) => {
-					switch (message.role) {
-						case "user":
-							return (
-								<p key={message.id} className="self-end bg-zinc-900 px-4 py-2 rounded-3xl max-w-4/5 text-zinc-200">{message.content}</p>
-							)
-						case "assistant":
-							return (
-								<div key={message.id} className="flex flex-col self-start gap-4 prose-invert w-4/5 prose">
-									{message.parts.map((part, index) => (
-										part.type === "text" && <Markdown remarkPlugins={[remarkGfm]} key={index}>{part.text}</Markdown>
-									))}
-								</div>
-							)
-					}
-				})}
-			</div>
+			<Messages messages={messages} />
 
 			{/* BOTTOM GRADIENT */}
-			<div className="bottom-0 left-0 fixed bg-gradient-to-t from-black to-transparent w-full h-24"></div>
+			<div className="bottom-4 left-0 fixed bg-gradient-to-t from-black to-transparent w-full h-32"></div>
+			<div className="bottom-0 left-0 fixed bg-black w-full h-4"></div>
 
 			{/* INPUT AREA */}
 			<form onSubmit={handleSubmit} className="bottom-4 left-1/2 fixed -translate-x-1/2">
@@ -70,3 +57,13 @@ export default function Home() {
 		</div>
 	);
 }
+
+// change model dropdown | or maybe just add a thinking button
+// add retry button on all messages
+// add a edit button to user messages
+// add loading state
+// error handling
+// make the input box get taller as more content is added
+// add support for images
+// add support for multiple chats
+// add new chat button
