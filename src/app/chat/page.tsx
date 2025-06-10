e853bb5e9487a3a3be2aa0a7cd02ccdf87b17e59
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-06-10 17:51:56 -0400

    redirect to auth page instead of home page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation"
 export default async function ChatPage() {
     const session = await auth()
 
-    if (!session?.user) redirect("/")
+    if (!session?.user) redirect("/auth")
 
     return (
         <ClientContainer />
