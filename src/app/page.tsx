c89dcae7d1a3672dd0e4edc6a478132e82e37fab
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-06-12 19:58:06 -0400

    added some test testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
+"use client"
+import { useState } from "react";
+
 import Link from "next/link";
 
 export default function Home() {
+	const [count, setCount] = useState(0)
+
 	return (
 		<div className="p-12">
-			<Link href="/chat" className="bg-zinc-800 px-4 py-1 border border-white/15 rounded-full">Chat Page</Link>
+			<Link data-testid="chat-page-button" href="/chat" className="bg-zinc-800 px-4 py-1 border border-white/15 rounded-full">Chat Page</Link>
 			<div className="flex flex-col gap-2 mt-12">
 				<p>Hero</p>
 				<p>Features</p>
 				<p>Why Choose Speek</p>
 				<p>FAQ</p>
+			</div>
+			<div>
+				<p data-testid="count-element">{count}</p>
+				<button onClick={() => setCount(prev => prev + 1)}>+1</button>
+				<button onClick={() => setCount(prev => prev - 1)}>-1</button>
 			</div>
 		</div>
 	);
