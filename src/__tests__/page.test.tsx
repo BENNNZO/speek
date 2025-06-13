8669b1672ddced8b81ed9bdce71a23dea90ca024
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-06-12 20:20:46 -0400

    made the tests simple

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
 
 describe(Home, () => {
@@ -9,35 +9,5 @@ describe(Home, () => {
         const buttonHref = screen.getByTestId("chat-page-button").getAttribute("href")
 
         expect(buttonHref).toEqual("/chat")
-    })
-
-    it("initial count is 0", () => {
-        render(<Home />)
-
-        const countElement = String(screen.getByTestId("count-element").textContent)
-
-        expect(countElement).toBe("0")
-    })
-
-    it("increment button adds 1 to count", () => {
-        render(<Home />)
-
-        const incrementButton = screen.getByRole("button", { name: "+1" })
-        fireEvent.click(incrementButton)
-
-        const countElement = String(screen.getByTestId("count-element").textContent)
-
-        expect(countElement).toBe("1")
-    })
-
-    it("decrement button removes 1 from count", () => {
-        render(<Home />)
-
-        const decrementButton = screen.getByRole("button", { name: "-1" })
-        fireEvent.click(decrementButton)
-
-        const countElement = String(screen.getByTestId("count-element").textContent)
-
-        expect(countElement).toBe("-1")
     })
 })
