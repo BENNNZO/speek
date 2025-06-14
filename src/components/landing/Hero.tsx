970af1c444b5c3a9ca3b0e6efad683438c9775f7
BENNNNZO <benjamin.phillips.webdev@gmail.com>
2025-06-13 21:38:34 -0400

    added little hero animation

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -6,14 +6,50 @@ export default function Hero() {
     return (
         <div className="place-items-center grid h-screen">
             <div className="flex flex-col gap-2">
-                <Header text="Speek AI" />
-                <SubHeader text="Intuitive, Sleek, Minimal." />
+                <div className="flex gap-12">
+                    <FadeLetters text="Speek" />
+                    <AI />
+                </div>
+                <FadeWords text="Intuitive, Sleek, Minimal." />
             </div>
         </div>
     )
 }
 
-function Header({ text }: { text: string }) {
+function AI() {
+    return (
+        <motion.div
+            initial={{ opacity: 0, x: -15, scale: 0.95 }}
+            animate={{ opacity: 1, x: 0, scale: 1, transition: { delay: 0.4 } }}
+            className="relative w-24"
+        >
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: [1, 1],
+                    background: ["rgb(0, 0, 0)", "rgb(255, 255, 255)"],
+                    width: ["7.5rem", "8rem"],
+                    height: ["7.5rem", "8rem"],
+                    transition: { repeat: 0, repeatType: "mirror", repeatDelay: 1, delay: 2 }
+                }}
+                className="top-1/2 left-1/2 absolute place-items-center grid rounded-3xl -translate-1/2"
+            >
+                <motion.p
+                    animate={{
+                        color: ["rgb(255, 255, 255)", "rgb(0, 0, 0)"],
+                        fontSize: ["6rem", "5rem"],
+                        transition: { repeat: 0, repeatType: "mirror", repeatDelay: 1, delay: 2 }
+                    }}
+                    className="top-1/2 left-1/2 absolute font-bold -translate-1/2"
+                >
+                    AI
+                </motion.p>
+            </motion.div>
+        </motion.div>
+    )
+}
+
+function FadeLetters({ text }: { text: string }) {
     const letters = [...text.split("")]
 
     return (
@@ -30,7 +66,7 @@ function Header({ text }: { text: string }) {
     )
 }
 
-function SubHeader({ text }: { text: string }) {
+function FadeWords({ text }: { text: string }) {
     const words = [...text.split(" ")]
 
     return (
@@ -52,7 +88,7 @@ function FadeInFromLeft({ text, delay, className }: { text: string, delay: numbe
         <motion.div
             initial={{ opacity: 0, x: -15, scale: 0.95 }}
             animate={{ opacity: 1, x: 0, scale: 1, transition: { delay } }}
-            className={`${className} ${text.trim() === "" ? "w-12" : ""}`}
+            className={className}
         >
             {text}
         </motion.div>
